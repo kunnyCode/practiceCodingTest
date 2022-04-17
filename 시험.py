6bb39d02a5cd5252e9278c2c2c4ec2750059594d
kunnie-code <kearnyyoon@gmail.com>
2022-04-17 23:31:59 +0900

    dfsbfs시작

--- 시험.py
+++ 시험.py
@@ -56,4 +56,108 @@
 # b = a.lower()
 # print(b)
 
-print(0 % 1)
+# print(0 % 1)
+
+import sys
+
+def input():
+
+    return sys.stdin.readline().rstrip()
+    
+N = int(input())
+inorder = list(map(int, input().split()))
+postorder = list(map(int, input().split()))
+inorder_idx = {val : idx for idx, val in enumerate(inorder)}
+# print(inorder_idx)
+
+
+
+def find_preorder(in_s, in_e, po_s, po_e):
+    if in_s > in_e or po_s > po_e: return
+    if in_s == in_e and po_s == po_e:
+        print(inorder[in_s], end = ' ')
+
+        return
+    
+    parent = postorder[po_e]
+    idx = inorder_idx[parent]
+    cnt = in_e - idx
+    
+    find_preorder(idx, idx, po_e, po_e)
+    find_preorder(in_s, idx - 1, po_s, po_e - in_e + idx - 1)
+    find_preorder(idx + 1, in_e, po_e - in_e + idx, po_e - 1)
+    
+find_preorder(0, N - 1, 0, N - 1)
+
+
+
+
+
+
+
+
+
+
+
+
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+S = input()
+
+for i in range(len(S)):
+    tmp_str = S[0:i]
+
+
+    tmp = S + tmp_str[::-1]
+    
+    if tmp == tmp[::-1]:
+        print(i)
+        exit()
+
+
+
+
+
+
+
+
+
+
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+    
+N, M, K = map(int, input().split())
+data = []
+
+
+for i in range(N):
+    x, y = map(int, input().split())
+
+
+    data.append([y, x])
+    
+data.sort()
+want_score = N * K
+present_score = 0
+answer = 0
+
+
+
+
+for node in data:
+    present_score += node[1]
+
+for node in data:
+    while want_score > present_score and node[1] != M:
+        answer += node[0]
+        node[1] += 1
+
+
+        present_score += 1
+        
+print(answer)
