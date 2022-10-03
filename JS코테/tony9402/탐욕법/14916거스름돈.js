a61fd857fb0c13e605ba16fd72e51dd0b90e3f75
kunnie-code <kearnyyoon@gmail.com>
2022-10-03 23:28:11 +0900

    거스름돈 모범답안 추가

--- JS코테/tony9402/탐욕법/14916거스름돈.js
+++ JS코테/tony9402/탐욕법/14916거스름돈.js
@@ -1,34 +1,66 @@
-const fs = require("fs");
-let input = fs.readFileSync("./dev/stdin/14916.txt").toString().trim();
+// https://www.acmicpc.net/problem/14916
 
-let answer = 0;
-let needed2 = 0;
-let needed5 = 0;
+// const fs = require("fs");
+// let input = fs.readFileSync("./dev/stdin/14916.txt").toString().trim();
 
-input = Number(input);
+// let answer = 0;
+// let needed2 = 0;
+// let needed5 = 0;
 
-if (input === 1 || input === 3) {
-  answer = -1;
-  console.log(answer);
-} else {
-  if (input % 2 === 0) {
-    let numOf5 = Math.floor(input / 5);
-    if (numOf5 % 2 !== 0) numOf5--;
+// input = Number(input);
 
-    needed2 = (input - numOf5 * 5) / 2;
-    needed5 = numOf5;
-  } else {
-    let numOf5 = Math.floor(input / 5);
-    if (numOf5 % 2 === 0) numOf5--;
-    needed2 = (input - numOf5 * 5) / 2;
-    needed5 = numOf5;
+// if (input === 1 || input === 3) {
+//   answer = -1;
+//   console.log(answer);
+// } else {
+//   if (input % 2 === 0) {
+//     let numOf5 = Math.floor(input / 5);
+//     if (numOf5 % 2 !== 0) numOf5--;
+
+//     needed2 = (input - numOf5 * 5) / 2;
+//     needed5 = numOf5;
+//   } else {
+//     let numOf5 = Math.floor(input / 5);
+//     if (numOf5 % 2 === 0) numOf5--;
+//     needed2 = (input - numOf5 * 5) / 2;
+//     needed5 = numOf5;
+//   }
+
+//   if (needed5 * 5 + needed2 * 2 !== input) {
+//     answer = -1;
+//   } else {
+//     answer = needed2 + needed5;
+//   }
+
+//   console.log(answer);
+// }
+
+//모범답안
+
+const input = require("fs")
+  .readFileSync("./dev/stdin/14916.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = Number(input[0]);
+
+function solution(n) {
+  let answer = 0;
+  if (n === 1 || n === 3) return -1;
+
+  while (n > 0) {
+    if (n % 2 === 1 || n % 5 === 0) {
+      answer += 1;
+      n -= 5;
+    } else if (n % 2 === 0) {
+      answer += 1;
+      n -= 2;
+    }
   }
 
-  if (needed5 * 5 + needed2 * 2 !== input) {
-    answer = -1;
-  } else {
-    answer = needed2 + needed5;
-  }
-
-  console.log(answer);
+  return answer;
 }
+
+const answer = solution(n);
+console.log(answer);
