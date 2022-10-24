32fc717d9d8b854c9d452fa96d92d909edcf8fd2
kunnie-code <kearnyyoon@gmail.com>
2022-10-25 00:44:50 +0900

    2

--- JS코테/블로그/bfs/2667단지번호붙이기.js
+++ JS코테/블로그/bfs/2667단지번호붙이기.js
@@ -19,20 +19,26 @@ const answerArr = [];
 const queue = [];
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < N; j++) {
+    // map에 값이 1이면서 한번도 방문하지 않은 경우
     if (map[i][j] === 1 && check[i][j] === 0) {
-      // check[i][j] = 1;
+      //총 단지수 수를 1 올린다.
       totalCnt += 1;
+
+      //해당 번지를 큐에 넣은 뒤 bfs를 돌면서 주변 인접한 단지를 다 찾는다.
       queue.push([i, j]);
       while (queue.length > 0) {
         const [x, y] = queue.shift();
-        // console.log(check[x][y], x, y);
+        // 한번도 방문하지 않은경우
         if (check[x][y] === 0) {
+          // 큐에서 꺼내서 방문을 확일할 때 방문을 안했을 경우 이 때 방문 처리를 한다.
+          // 연결된 단지 수를 1 올린다.
           check[x][y] = 1;
           answerCnt += 1;
           for (let k = 0; k < 4; k++) {
             const a = x + dx[k];
             const b = y + dy[k];
             if (a > -1 && a < N && b > -1 && b < N) {
+              // 1인 경우 단지가 있다고 판단, 큐에 넣는다.
               if (map[a][b] === 1) {
                 queue.push([a, b]);
                 // console.log(queue);
